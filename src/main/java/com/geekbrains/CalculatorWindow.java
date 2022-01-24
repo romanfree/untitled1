7b3fdb7aa2450285b25bfa137f4e6049f9bb76fb
roman <romanfree1311@gmail.com>
2022-01-24 19:18:45 +0300

    Initial commit

---
+++ src/main/java/com/geekbrains/CalculatorWindow.java
@@ -0,0 +1,13 @@
+package com.geekbrains;
+
+import javax.swing.*;
+
+public class CalculatorWindow extends JFrame {
+    public CalculatorWindow(){
+        setTitle("Калькулятор");
+        setBounds(300,300,400,400);
+        setVisible(true);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        System.out.println("ddddd");
+    }
+}
